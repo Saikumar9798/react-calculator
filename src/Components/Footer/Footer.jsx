274d301cf27f2ff6746fd3318fe68b58d24a2242
Saikumar <gv.saikumar.97@gmail.com>
2021-08-15 16:09:10 +0530

    Updates

--- src/Components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./index.css";
-
-const Footer = () => {
-    return <div className="footer">Built and Maintained 🚧 by Saikumar!</div>;
-}
-
-export default Footer
